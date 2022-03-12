ad868dc0b38535c3cf6d6524bbfd82af8cb6b422
Steven Cook <stevenjamescook@gmail.com>
2022-03-12 20:47:17 +0000

    refactor

--- api-tests/lib/api.test.ts
+++ api-tests/lib/api.test.ts
@@ -4,19 +4,18 @@ import {booking} from '../../test-framework-lib/test-factories/index';
 
 const API_BASE_URL = process.env['API_BASE_URL'];
 
-let response: any;
-
 describe('Hotel Room Booking System', () => {
   describe('Scenario: Listing all bookings', () => {
+    let response: any;
     beforeEach(async () => {
       response = await getBookings();
     });
     describe('Given there are already bookings in the system', () => {
-      describe('Then all bookings should be listed', () => {
-        it('And the response code should be 200 OK', async () => {
-          expect(response.statusCode).toEqual(200);
-          expect(response.body.length).toBeGreaterThan(0);
-        });
+      it('Then all bookings should be listed', async () => {
+        expect(response.body.length).toBeGreaterThan(0);
+      });
+      it('And the response code should be 200 OK', async () => {
+        expect(response.statusCode).toEqual(200);
       });
     });
   });
@@ -24,6 +23,7 @@ describe('Hotel Room Booking System', () => {
   describe('Scenario: Adding a booking to the system', () => {
     let bookings: any;
     let numberOfBookings: number;
+    let response: any;
     beforeEach(async () => {
       bookings = await getBookings();
       numberOfBookings = bookings.body.length;
@@ -31,24 +31,39 @@ describe('Hotel Room Booking System', () => {
     });
     describe('Given the users fills all required fields correctly', () => {
       describe('When the booking is saved', () => {
-        describe('Then they should be able to see the new booking', () => {
-          it('And the response code shoud be a 201 Created', async () => {
-            const newNumBookings = (await getBookings()).body.length;
-            expect(newNumBookings).toEqual(numberOfBookings + 1);
-            expect(response.statusCode).toEqual(201);
-          });
+        it('Then they should be able to see the new booking', async () => {
+          const newNumBookings = (await getBookings()).body.length;
+          expect(newNumBookings).toEqual(numberOfBookings + 1);
+        });
+        it('And the response code shoud be a 201 Created', async () => {
+          expect(response.statusCode).toEqual(201);
         });
       });
     });
   });
 
   describe('Scenario: Deleting a booking from the system', () => {
+    let bookings: any;
+    let numberOfBookings: number;
+    let bookingId: number;
+    let response: any;
+
+    beforeEach(async () => {
+      const createResp = await createBooking();
+      bookings = await getBookings();
+      numberOfBookings = bookings.body.length;
+      bookingId = createResp.body.bookingid;
+      response = await deleteBooking(bookingId);
+    });
+
     describe('Given there is a booking that needs deleting ', () => {
       describe('When the user deletes the booking', () => {
-        describe('Then the booking should be removed', () => {
-          it('And the response code should be 200 OK', () => {
-            expect(response.statusCode).toEqual(200);
-          });
+        it('Then the booking should be removed', async () => {
+          const newNumBookings = (await getBookings()).body.length;
+          expect(newNumBookings).toEqual(numberOfBookings - 1);
+        });
+        it('And the response code should be 200 OK', async () => {
+          expect(response.statusCode).toEqual(200);
         });
       });
     });
@@ -66,5 +81,11 @@ const createBooking = async () => {
   return await request(API_BASE_URL)
       .post('/booking')
       .send(fakeBooking)
+      .set('Accept', 'application/json');
+};
+
+const deleteBooking = async (id: number) => {
+  return await request(API_BASE_URL)
+      .delete(`/booking/${id}`)
       .set('Accept', 'application/json');
 };
